a0047e6add4766c9b7fed7fcfff3f6a7b81e5d57
huang02252001 <146065198+huang02252001@users.noreply.github.com>
2023-10-31 00:59:19 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+main()
+{
+    int hour,minute;
+    scanf("%d %d", &hour,&minute);
+    float angleh,anglem;
+
+    angleh=360*(float)hour/12+(30*(float)minute/60);//1 hr=360/12=30.
+    anglem=360*(float)minute/60;
+
+    if(fabs(angleh-anglem)<360-fabs(angleh-anglem))printf("%.3f",fabs(angleh-anglem));
+    else printf("%.3f",360-fabs(angleh-anglem));
+
+
+
+
+}
 
